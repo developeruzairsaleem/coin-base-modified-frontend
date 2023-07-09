1d40f89a1e327669ff45ace5da67cba4d7c18ec4
Uzair Saleem <uzairsaleemdev@gmail.com>
2023-07-10 03:51:33 +0500

    uzairkhan

--- src/api/external.js
+++ src/api/external.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 
-// const NEWS_API_ENDPOINT=`https://newsapi.org/v2/everything?q=business AND bitcoin&sortBy=publishedAt&language=en&apiKey=${NEWS_API_KEY}`;
+
 const CRYPTO_API_ENDPOINT="https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false";
-const NEWS_API_ENDPOINT= "http://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
+const NEWS_API_ENDPOINT= "https://saurav.tech/NewsAPI/top-headlines/category/business/us.json"
 
 export const getNews= async()=>{
 let response;
