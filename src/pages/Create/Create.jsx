2d178208f390245d82d8801452cd6fb6df6cd460
Uzair Saleem <uzairsaleemdev@gmail.com>
2024-04-12 11:38:38 -0700

    added rich text editor

--- src/pages/Create/Create.jsx
+++ src/pages/Create/Create.jsx
@@ -1,120 +1,173 @@
+// import React from 'react';
+// import ReactDOM from 'react-dom';
+// import {Editor, EditorState} from 'draft-js';
+ 
+// function MyEditor() {
+//   const [editorState, setEditorState] = React.useState(
+//     EditorState.createEmpty()
+//   );
+ 
+//   const editor = React.useRef(null);
+ 
+//   function focusEditor() {
+//     editor.current.focus();
+//   }
+ 
+//   React.useEffect(() => {
+//     focusEditor()
+//   }, []);
+ 
+//   return (
+//     <div onClick={focusEditor}>
+//       <Editor
+//         ref={editor}
+//         editorState={editorState}
+//         onChange={editorState => setEditorState(editorState)}
+//       />
+//     </div>
+//   );
+// }
 
-import {useState } from "react";
+
+
+import React,{useState } from "react";
 import { submitBlog } from "../../api/internal";
 import { useSelector } from "react-redux"; 
 import TextInput from "../../components/TextInput/TextInput";
 import styles from "./Create.module.css"
 import { useNavigate } from "react-router-dom";
+import {Editor, EditorState} from "draft-js";
+
+
 // 
 
 function Create(){
 
-const navigate=useNavigate()
+  const [title,setTitle]=useState("");
+  const [content,setContent]=useState("");
+  const [description,setDescription]=useState("");
+  const [photo,setPhoto]=useState("");
+  const [editorState, setEditorState] = useState(EditorState.createEmpty());
+  const editor = React.useRef(null);
+  const author = useSelector((state)=>state.user._id);
+  const navigate=useNavigate();
+  //----------------------------------
+  // submitting the data to the server
+  //----------------------------------
 
-const handleSubmit=async ()=>{
- const contentStruct = {
-    "text":'some random text',
-    "bold":true,
-    "italic":true,
-    "underline":true,
-    "fontsize":10
+
+
+  const focusEditor=()=>{
+    editor.current.focus();
+  }
+  const handleSubmit = async () => {
+
+    //---------------------------------------------------
+    // storing the data in object to send to the backend
+    //---------------------------------------------------
+
+    let data = {
+      title,content,photo,author,description
+    };
+
+    const response =  await submitBlog(data);
+
+    if(response.status === 201){
+      navigate("/");
+    }
+
   }
 
-let str= content.replace(/\n/g,'|n|')
 
-  let data={
-    title,content:str,photo,author,description
+  //----------------------------------------------------------------------------
+  // handle the change in the photo and setting the state with new BASE64 String
+  //----------------------------------------------------------------------------
+  const photoChange=(e)=>{
+    const file = e.target.files[0]
+    const reader= new FileReader();
+    reader.readAsDataURL(file);
+    reader.onloadend=()=>{
+      setPhoto(reader.result)
+    }
+
   }
-  const response=  await submitBlog(data)
-if(response.status===201){
-  navigate("/")
-}
-
-}
-
-const photoChange=(e)=>{
-const file = e.target.files[0]
-const reader= new FileReader();
- reader.readAsDataURL(file);
- reader.onloadend=()=>{
-  setPhoto(reader.result)
- }
-
-}
-
-
-const [title,setTitle]=useState("");
-const [content,setContent]=useState("");
-const [description,setDescription]=useState("");
-
-const [photo,setPhoto]=useState("");
-const author = useSelector((state)=>state.user._id)
 
 
 
 
-return (
-    <div className={styles.wrapper}>
-    <div className={styles.header} >Create a blog</div>
-    <TextInput
-      type="text"
-      name="title"
-      value={title}
-      onChange={(e)=>setTitle(e.target.value)}
-      placeholder="title"
-      style={{width:"60%"}}
-    />
-   
 
 
+  return (
+      <div className={styles.wrapper}>
+        <div  className={styles.header} >Create a blog</div>
+        <TextInput
+          type="text"
+          name="title"
+          value={title}
+          onChange={(e)=>setTitle(e.target.value)}
+          placeholder="title"
+          style={{width:"60%"}}
+        />
+     
 
-    <textarea 
-    name="description"
-    value={description}
-    className={styles.description}
-    placeholder="Your description goes here ..."
-    onChange={(e)=>{
+
+{/*
+      <textarea 
+      name="description"
+      value={description}
+      className={styles.description}
+      placeholder="Your description goes here ..."
+      onChange={(e)=>{
+    
+      setDescription(e.target.value)
+
+
+        }}
+     />
+
+
+     <textarea 
+      name="content"
+      value={content}
+      className={styles.content}
+      // maxLength={400}
+      placeholder="Your content goes here ..."
+      onChange={(e)=>{
+    
+      setContent(e.target.value)
   
-    setDescription(e.target.value)
-    console.log(description)
 
 
-      }}
-   />
+        }}
+     />
+     <div className={styles.photoPrompt} >
+        <p>Choose a photo</p>
+        <input
+         type="file"
+         name="photo"
+         id="photo"
+         onChange={photoChange}
+         accept="image/jpg, image/jpeg, image/png"
+              />
+        {photo!==""?<img width={100} src={photo}/>:""}*/}
+     {/*</div>*/}
 
 
-   <textarea 
-    name="content"
-    value={content}
-    className={styles.content}
-    // maxLength={400}
-    placeholder="Your content goes here ..."
-    onChange={(e)=>{
-  
-    setContent(e.target.value)
-console.log(content)
-
-
-      }}
-   />
-   <div className={styles.photoPrompt} >
-    <p>Choose a photo</p>
-    <input
-     type="file"
-     name="photo"
-     id="photo"
-     onChange={photoChange}
-     accept="image/jpg, image/jpeg, image/png"
+        <div width={"100px"} height={"200px"} onClick={ focusEditor } >
+          <Editor
+            ref={editor}
+            editorState={editorState}
+            onChange={eState=>setEditorState(eState)}
           />
-          {photo!==""?<img width={100} src={photo}/>:""}
-   </div>
-   
+        </div>
+     
 
-  <button className={styles.submit} disabled={title===""||content===""||photo===""} onClick={handleSubmit} >Submit</button>
+     <button className={styles.submit} disabled={title===""||content===""||photo===""} onClick={handleSubmit} >Submit</button>
 
 
+      
     </div>
-)
+  )
 
 }
 
